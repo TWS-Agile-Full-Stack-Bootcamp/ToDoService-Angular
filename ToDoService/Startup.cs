246b824bb68aa39895bb2ffcea2bca9b50952f7d
ChenXu <xuchen@thoughtworks.com>
2020-12-03 13:30:52 +0800

    feat: update controller

--- ToDoService/Startup.cs
+++ ToDoService/Startup.cs
@@ -27,15 +27,6 @@ namespace ToDoService
                 configuration.RootPath = "ClientApp/dist";
             });
             services.AddSwaggerGen();
-            services.AddCors(options =>
-            {
-                options.AddPolicy("AllowAll", p =>
-                {
-                    p.AllowAnyOrigin()
-                        .AllowAnyHeader()
-                        .AllowAnyMethod();
-                });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
